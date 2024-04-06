2a688575c19a7e43a564aaa43e5b64a25cf28f75
Laurentius <laurentiuskevin44@gmail.com>
2024-04-07 01:57:44 +0700

    Update sequelize.ts

--- src/entity/sequelize.ts
+++ src/entity/sequelize.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USERNAME || "postgres",
   process.env.DB_PASSWORD || "postgres",
   {
-    host: process.env.DB_PASSWORD || "localhost",
+    host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
     logging: (str) => {
       console.info(str);
