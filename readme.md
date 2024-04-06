b34a708f4e0c3f235c83a9809fcb12b23f199f76
Laurentius <laurentiuskevin44@gmail.com>
2024-04-06 23:24:51 +0700

    add readme and env

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Bookstore
+
+### Requirements
+
+1. NodeJs >= 20
+2. DB Postgresql
+
+### How to install
+
+1. clone repository
+2. create .env file based on .env.example and adjust it based on your db configuration
+3. npm install
+4. npm run dev
--- src/entity/sequelize.ts
+++ src/entity/sequelize.ts
@@ -1,11 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("hyperhire_bookstore", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-  logging: (str) => {
-    console.info(str);
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "hyperhire_bookstore",
+  process.env.DB_USERNAME || "postgres",
+  process.env.DB_PASSWORD || "postgres",
+  {
+    host: process.env.DB_PASSWORD || "localhost",
+    dialect: "postgres",
+    logging: (str) => {
+      console.info(str);
+    },
+  }
+);
 
 export default sequelize;
